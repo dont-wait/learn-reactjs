88bc4c52d7dfbe249a65aac5d81aff0b0dd1fdf2
dont-wait <nguyentansang15042005@gmail.com>
2025-05-14 00:03:01 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,12 +1,19 @@
 //component = html + css + js
 import './style.css'
 const MyComponent = () => {
+    const value = [1, 2, 3];
+    // const value = {
+    //     name: "abc",
+    //     age: 18,
+    //     address: "Hà Nội",
+    // }
     return (
         <>
+            <div>{console.log("Hello world")}</div>
             <div
                 style={{ borderRadius: "10px", color: "blue" }
                 }>dontwait update v2</div>
-            <div className="child">dontwait update v3</div>
+            <div className="child"> {JSON.stringify(value)} dontwait update v3</div>
         </>
     );
 }
