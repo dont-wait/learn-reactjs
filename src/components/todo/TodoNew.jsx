77d5e84f76764470a24926fe56fe09b6860a689a
dont-wait <nguyentansang15042005@gmail.com>
2025-05-15 18:46:05 +0700

    chore: add vide 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,9 +1,20 @@
 const TodoNew = (props) => {
     const { addNewToDo, name } = props;
+
+    const handleClick = () => {
+        alert("Click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log("handleOnchanged: ", name)
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" placeholder='Input a new work' />
-            <button>Add</button>
+            <input type="text" placeholder='Input a new work'
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button onClick={handleClick}>Add</button>
             {addNewToDo(name)}
         </div>
     )
