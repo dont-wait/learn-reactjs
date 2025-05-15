7a81f772984fd9904cf8a2639fb9655003ddf77f
dont-wait <nguyentansang15042005@gmail.com>
2025-05-15 17:16:31 +0700

    chore: add video 34

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import TodoContent from './components/todo/TodoContent'
 import TodoNew from './components/todo/TodoNew'
 import reactLogo from './assets/react.svg'
 const App = () => {
-  const dontwait = "hello"
+  const name = "dontwait";
+  const age = 19;
+  const data = {
+    address: "hcm",
+    country: "vietnam"
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoContent />
+      <TodoContent
+        name={name}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
         <img src={reactLogo} alt="" className='logo' />
       </div>
--- src/components/todo/TodoContent.jsx
+++ src/components/todo/TodoContent.jsx
@@ -1,6 +1,20 @@
-const TodoContent = () => {
+const TodoContent = (props) => {
+    console.log("props: ", props)
+
+    //c1
+    const { name, age, data } = props
+
+    //c2
+    // const name = props.name
+    // const age = props.age
+    // const data = props.data
+
+    //c3 
+    //thay props bang key can lay vd: {name} {age}
+
     return (
         <div className='todo-content'>
+            <div>My name is {JSON.stringify(data)}</div>
             <div>Learning React</div>
             <div>Learning Angular</div>
         </div>
