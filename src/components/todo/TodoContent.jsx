c4fcdbb5592944eafe71caab1e6915b1c192bc8d
dont-wait <nguyentansang15042005@gmail.com>
2025-05-15 16:41:53 +0700

    chore: add video 33

---
+++ src/components/todo/TodoContent.jsx
@@ -0,0 +1,10 @@
+const TodoContent = () => {
+    return (
+        <div className='todo-content'>
+            <div>Learning React</div>
+            <div>Learning Angular</div>
+        </div>
+    )
+}
+
+export default TodoContent
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+    return (
+        <div className='todo-new'>
+            <input type="text" placeholder='Input a new work' />
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default TodoNew
