70900a93997815fbefa7e66828cfcf270c2cc5b7
dontwait <nguyentansang15042005@gmail.com>
2025-06-27 15:57:30 +0700

    video 50: Navlink

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
     //anchor
     return (
         <ul>
-            <li><Link className="active" to="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/products">Product</Link></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/products">Product</NavLink></li>
         </ul>
     )
 }
